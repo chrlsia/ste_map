7fd27e02469accf6d444364992ff7db2cb044d52
Chris Siannas <xrsia@otenet.gr>
2021-12-19 19:38:53 +0200

    Another way to create a map

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main(){
-	colors:= map[string]string{
-		"red":"#ff0000",
-		"green":"#4bf745",
-	}
+	// colors:= map[string]string{
+	// 	"red":"#ff0000",
+	// 	"green":"#4bf745",
+	// }
+
+	var colors map[string]string
 
 	fmt.Println(colors)
 }
