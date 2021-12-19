213d423633ac559affb5a3f11e4c855bbddb502a
Chris Siannas <xrsia@otenet.gr>
2021-12-19 19:43:46 +0200

    Manipulating maps

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ func main(){
 	// 	"green":"#4bf745",
 	// }
 
-	var colors map[string]string
+	// var colors map[string]string
+
+	colors:=make(map[string]string)
+
+	colors["white"] = "#ffffff"
+
+	delete(colors,"white")
 
 	fmt.Println(colors)
 }
